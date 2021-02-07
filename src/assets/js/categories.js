f84d910cc834ed13eb5abadf1304c9e98d6c8e25
Valentin Doche <contact@valentindoche.com>
2021-02-07 20:57:59 +0100

    categories json

---
+++ src/assets/js/categories.js
@@ -0,0 +1,148 @@
+const categories = {
+    0: {
+        "title" : "General Knowledge",
+        "image" : "",
+        "categories_id": 0,
+        "best_categories": true
+    },
+    1 : {
+        "title" : "Entertainment : Books",
+        "image" : "",
+        "categories_id": 1,
+        "best_categories": false
+    },
+    2: {
+        "title" : "Entertainment : Cartoon",
+        "image" : "",
+        "categories_id": 2,
+        "best_categories": true
+    },
+    3: {
+        "title" : "Entertainment : Comics",
+        "image" : "",
+        "categories_id": 3,
+        "best_categories": false
+    },
+    4: {
+        "title" : "Entertainment : Film",
+        "image" : "",
+        "categories_id": 4,
+        "best_categories": false
+    },
+    5: {
+        "title" : "Entertainment : Japanese Anime & Manga",
+        "image" : "",
+        "categories_id": 5,
+        "best_categories": false
+    },
+    6: {
+        "title" : "Entertainment : Music",
+        "image" : "",
+        "categories_id": 6,
+        "best_categories": false
+    },
+    7: {
+        "title" : "Entertainment : Musicals & Theatres",
+        "image" : "",
+        "categories_id": 7,
+        "best_categories": false
+    },
+    8: {
+        "title" : "Entertainment : Television",
+        "image" : "",
+        "categories_id": 8,
+        "best_categories": false
+    },
+    9: {
+        "title" : "Entertainment : Video Games",
+        "image" : "",
+        "categories_id": 9,
+        "best_categories": false
+    },
+    10: {
+        "title" : "Entertainment : Board Games",
+        "image" : "",
+        "categories_id": 10,
+        "best_categories": true
+    },
+    11: {
+        "title" : "Science & Nature",
+        "image" : "",
+        "categories_id": 11,
+        "best_categories": false
+    },
+    12: {
+        "title" : "Science: Computers",
+        "image" : "",
+        "categories_id": 12,
+        "best_categories": false
+    },
+    13: {
+        "title" : "Science: Gadgets",
+        "image" : "",
+        "categories_id": 13,
+        "best_categories": false
+    },
+    14: {
+        "title" : "Science: Mathematics",
+        "image" : "",
+        "categories_id": 14,
+        "best_categories": false
+    },
+    15: {
+        "title" : "Mythology",
+        "image" : "",
+        "categories_id": 15,
+        "best_categories": false
+    },
+    16: {
+        "title" : "Sports",
+        "image" : "",
+        "categories_id": 16,
+        "best_categories": false
+    },
+    17: {
+        "title" : "Geography",
+        "image" : "",
+        "categories_id": 17,
+        "best_categories": false
+    },
+    18: {
+        "title" : "History",
+        "image" : "",
+        "categories_id": 18,
+        "best_categories": false
+    },
+    19: {
+        "title" : "Politics",
+        "image" : "",
+        "categories_id": 19,
+        "best_categories": false
+    },
+    20: {
+        "title" : "Art",
+        "image" : "",
+        "categories_id": 20,
+        "best_categories": false
+    },
+    21: {
+        "title" : "Celebrities",
+        "image" : "",
+        "categories_id": 21,
+        "best_categories": false
+    },
+    22: {
+        "title" : "Animals",
+        "image" : "",
+        "categories_id": 22,
+        "best_categories": false
+    },
+    23: {
+        "title" : "Vehicles",
+        "image" : "",
+        "categories_id": 23,
+        "best_categories": false
+    }
+}
+
+$('.best-categories').clone().appendTo(".best_categories-all")
